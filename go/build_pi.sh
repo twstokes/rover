572db25c879a80557bd5742bec7e0e06eadde7bd
Tanner Stokes <tanner@tannr.com>
2018-04-01 19:40:33 -0400

    Project cleanup

--- go/build_pi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-env GOOS=linux GOARCH=arm GOARM=6 go build -o rover
--- go/cmd/udp/main.go
+++ go/cmd/udp/main.go
@@ -8,11 +8,11 @@ import (
 	"os/signal"
 	"time"
 
-	"github.com/twstokes/rover/go/mcu"
+	"github.com/twstokes/rover/go/pkg/mcu"
 )
 
 var (
-	serialPort = flag.String("serialPort", "/dev/ttyACM0", "serial port")
+	serialPort = flag.String("serialPort", "/dev/ttyACM0", "serial port") // default port on RPi
 	baudRate   = flag.Int("baud", 115200, "serial baud rate")
 	udpPort    = flag.Int("udpPort", 8000, "udp port")
 	servos     = flag.Int("servos", 4, "number of servos")
