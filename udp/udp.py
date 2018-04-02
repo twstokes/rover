03651414c9f89f311c8027e9cd39948da209f71c
Tanner Stokes <tanner@tannr.com>
2018-04-01 20:17:15 -0400

    Python UDP code

--- udp/udp.py
+++ udp/udp.py
@@ -6,13 +6,13 @@ UDP_PORT = 8000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-val = 0
+val = 60
 
-while val <= 180:
+while val <= 90:
     values = [255, 1, val]
 
     sock.sendto(bytes(values), (UDP_IP, UDP_PORT))
 
     val += 1
-    time.sleep(.01)
+    time.sleep(1)
 
