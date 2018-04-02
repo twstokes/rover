5d576ef9093564c71aa910e8f3f935fb5fea306a
Tanner Stokes <tanner@tannr.com>
2018-04-01 20:16:32 -0400

    Updated Python

--- controller/controller_udp.py
+++ controller/controller_udp.py
@@ -15,22 +15,106 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 _joystick = pygame.joystick.Joystick(0)
 _joystick.init()
 
+def writeVal(id, val):
+	print("ID: "+str(id))
+	print("Val: "+str(val))
+
+	values = [255, id, val]
+	sock.sendto(bytes(values), (UDP_IP, UDP_PORT))
+
+def processAxis(axis, trim, limitLow, limitHigh, invert):
+	rawVal = _joystick.get_axis(axis)
+
+	# round
+	axisVal = round(rawVal*100)
+
+	# map to 0-90
+	axisVal *= .9
+
+	# make an integer
+	axisVal = int(axisVal + 90)
+
+	if invert:
+		# invert axis
+		axisVal = 180 - axisVal
+
+	# trim
+	axisVal += trim
+
+	# safety limits
+	if axisVal < limitLow:
+		axisVal = limitLow
+	
+	if axisVal > limitHigh:
+		axisVal = limitHigh
+
+	return axisVal
+
+def processThrottle(axis, trim, limitLow, limitHigh, invert):
+	rawVal = _joystick.get_axis(axis)
+
+	# round
+	axisVal = round(rawVal*100)
+
+	# make positive
+	axisVal += 100
+
+	# map to 0-90
+	axisVal *= .9
+
+	# make an integer
+	axisVal = int(axisVal + 90)
+
+	if invert:
+		# invert axis
+		axisVal = 180 - axisVal
+	
+	# trim
+	axisVal += trim
+
+	# safety limits
+	if axisVal < limitLow:
+		axisVal = limitLow
+	
+	if axisVal > limitHigh:
+		axisVal = limitHigh
+
+	return axisVal
+
+# todo - make trim remap rather than cut so we still get full range
+
+steeringVal = 90
+throttleVal = 90
+camPan = 90
+camTilt = 90
+
 while True:
 	for event in pygame.event.get():
-		leftXAxis = _joystick.get_axis(0)
-		# round
-		leftXAxis = round(leftXAxis*100)
-		# adjust to positive number
-		leftXAxis += 100
-		# map to 0-180
-		leftXAxis *= .9
-		# make an integer
-		leftXAxis = int(leftXAxis)
- 
+		# steering
+		if event.axis == 0:
+			steeringVal = processAxis(0, 20, 50, 130, True)
+		
+		# throttle
+		if event.axis == 5:
+			throttleVal = processThrottle(5, 0, 0, 180, True)
 
-	print(leftXAxis)
+		# reverse
+		if event.axis == 4:
+			throttleVal = processThrottle(4, 0, 0, 180, False)
 
-	values = [255, 1, leftXAxis]
-	sock.sendto(bytes(values), (UDP_IP, UDP_PORT))
- 
+		# # cam pan
+		# if event.axis == 2:
+		# 	camPan = processAxis(2, 7, 50, 130, True)
+		
+		# cam tilt
+		if event.axis == 3:
+			camTilt = processAxis(3, 30, 90, 150, False)
+
+	writeVal(1, steeringVal)
+	writeVal(2, throttleVal)
+	# writeVal(3, camPan)
+	writeVal(4, camTilt)
+
+	# time.sleep(0.05)
+
 	clock.tick(30)
